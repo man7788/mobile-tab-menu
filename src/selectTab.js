283213cc6d3f73adaec5d3b6176a05c6b0201342
man7788 <jerman1841@gmail.com>
2022-12-09 00:10:25 +0800

    Add click highlight bar

--- src/selectTab.js
+++ src/selectTab.js
@@ -6,24 +6,34 @@ function clickTab() {
     item.addEventListener('click', (e) => {
       let target = e.composedPath();
       target = target[target.length - 6];
-      divList.forEach((grey) => {
-        if (grey.innerText !== target.innerText) {
-          setStyleGrey(grey);
-        }
+      divList.forEach((nonTarget) => {
+        setStyleGrey(nonTarget);
+        setBarHidden(nonTarget.childNodes[5]);
       });
       setStyleBlack(target);
+      setBarAppear(target.childNodes[5]);
     });
   });
 }
 
 function setStyleGrey(tab) {
   const tabDiv = tab;
-  tabDiv.style.filter = 'invert(70%)';
+  tabDiv.style.filter = 'invert(60%)';
 }
 
 function setStyleBlack(tab) {
   const tabDiv = tab;
   tabDiv.style.filter = 'invert(0)';
+}
+
+function setBarAppear(tab) {
+  const tabDiv = tab;
+  tabDiv.style.display = 'block';
+}
+
+function setBarHidden(tab) {
+  const tabDiv = tab;
+  tabDiv.style.display = 'none';
 }
 
 export default clickTab;
